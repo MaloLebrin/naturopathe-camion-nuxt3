018957c518abef8775f7800d3f273c5a167ee2c4
MaloLebrin <malolebrin@gmail.com>
2022-11-14 23:21:31 +0100

    feat: create categories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('./colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,6 +10,26 @@ module.exports = {
     "./nuxt.config.{js,ts}",
     "./app.vue"
   ],
+  safelist: [
+    ...(Object.keys(colors).reduce((acc, name) => {
+      acc.push(`border-${name}-600`)
+      acc.push(`border-${name}-600`)
+      acc.push(`bg-${name}-800`)
+      acc.push(`bg-${name}-700`)
+      acc.push(`bg-${name}-600`)
+      acc.push(`bg-${name}-500`)
+      acc.push(`bg-${name}-400`)
+      acc.push(`bg-${name}-100`)
+      acc.push(`bg-${name}-50`)
+      acc.push(`hover:bg-${name}-600`)
+      acc.push(`hover:bg-${name}-800`)
+      acc.push(`bg-${name}-100`)
+      acc.push(`ring-${name}-600`)
+      acc.push(`focus:outline-${name}-800`)
+      return acc
+    }, [])),
+  ],
+
   theme: {
     extend: {},
   },
