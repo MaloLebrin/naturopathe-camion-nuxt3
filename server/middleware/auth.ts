ff3e22f75ba926e511572385cf15728e08dac00f
MaloLebrin <malolebrin@gmail.com>
2022-11-24 11:47:15 +0100

    fix: remove unsed files

--- server/middleware/auth.ts
+++
@@ -1,5 +0,0 @@
-import { serverSupabaseUser, serverSupabaseClient, serverSupabaseServiceRole } from '#supabase/server'
-
-export default defineEventHandler(async (event) => {
-  // console.log(process.env.SUPABASE_SERVICE_KEY, '<==== process.env.SUPABASE_SERVICE_KEY')
-})
