e477d26b3b5ba17723e91fa23030943542e70d9d
malolebrin <malolebrin@gmail.com>
2023-01-02 16:38:57 +0100

    🩹 cypress url

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     env: {
-      FRONT_URL: process.env.FRONT_URL || 'http://localhost:3001/',
+      FRONT_URL: process.env.FRONT_URL || 'http://localhost:3000/',
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
