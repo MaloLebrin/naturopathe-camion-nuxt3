aec679fd2b9b959fce230435cf8547db5a8331c2
malolebrin <malolebrin@gmail.com>
2023-01-03 15:00:14 +0100

    🧑‍💻 Add commands to login and get Data by cy

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -29,30 +29,3 @@ Cypress.Commands.add('login', () => {
 Cypress.Commands.add('dataCy', (value: string) => {
   return cy.get(`[data-cy=${value}]`)
 })
-
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      login(email: string, password: string): Chainable<void>
-      dataCy(value: string): Chainable<Element>
-      // drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-      // dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-      // visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-      // getByDataCy(value: string): Chainable<Element>
-    }
-  }
-}
