019f55783929e62205a531c6c0c6777924e21d93
malolebrin <malolebrin@gmail.com>
2023-01-03 15:00:34 +0100

    🩹 Eslint errors in create article page

--- pages/Admin/articles/create.vue
+++ pages/Admin/articles/create.vue
@@ -1,21 +1,20 @@
 <template>
-  <AdminPageWrapper title="Créer un nouvel article">
-    <ArticleForm />
-  </AdminPageWrapper>
-  </template>
-  
-  <script setup lang="ts">  
-  definePageMeta({
-    layout: 'admin',
-    middleware: [
-      'nav-guard-middleware',
-      'categories-middleware'
-    ],
-    isAuth: true,
-  })
-  
-  useHead({
-    title: `Naturopathe-Camion | Créer un article`,
-  })
-  </script>
-  
+<AdminPageWrapper title="Créer un nouvel article">
+  <ArticleForm />
+</AdminPageWrapper>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'admin',
+  middleware: [
+    'nav-guard-middleware',
+    'categories-middleware',
+  ],
+  isAuth: true,
+})
+
+useHead({
+  title: 'Naturopathe-Camion | Créer un article',
+})
+</script>
